cb8afc0b79dd674a026eb353674911602c5679c0
Daniel2711 <detaddeo.d@gmail.com>
2021-02-07 16:43:57 +0100

    Chiama API boolean per volte necessarie

--- app.js
+++ app.js
@@ -3,48 +3,57 @@ new Vue({
     data:{
         test:'test',
         arrayEmail: [],
-        NumMaxEmail: 10
+        numMaxEmail: 20
     },
 
     beforeMount() {
-        
-        console.log('Crea 10 email');
 
-        let emailObject = {
-            name: '',
-            mail: ''
+        // chiedi 10 email random
+        for (let i = 0; i < this.numMaxEmail; i++) {
+            this.getEmailRandom();
         }
+    },
 
-        axios
-            .all([
-                axios.get('https://flynn.boolean.careers/exercises/api/random/name'),
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            ])
-            .then(axios.spread((nome, mail) => {
+    methods: {
+        getEmailRandom: function(){
+            // Call to new email e name random
+            let emailObject = {
+                name: '',
+                mail: ''
+            }
+
+            // avvio chiamate API
+            axios
+                .all([
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/name'),
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                ])
                 
-                // value from axios get
-                const valueName = nome.data.response;
-                const valueMail = mail.data.response;
+                .then(axios.spread((nome, mail) => {
+                    
+                    // value from axios get
+                    const valueName = nome.data.response;
+                    const valueMail = mail.data.response;
 
-                // print in console value from axios get
-                console.log('Name: ' + valueName);
-                console.log('Mail: ' + valueMail);
+                    // print in console value from axios get
+                    // console.log('Name: ' + valueName);
+                    // console.log('Mail: ' + valueMail);
 
-                // edit emailObject value from value of axios get
-                emailObject.name = valueName;
-                emailObject.mail = valueMail;
+                    // edit emailObject value from value of axios get
+                    emailObject.name = valueName;
+                    emailObject.mail = valueMail;
 
-                // print ckeck emailObject
-                console.log(emailObject);
+                    // print ckeck emailObject
+                    console.log(emailObject);
 
-                // add to emailObject in the array
-                this.arrayEmail.push(emailObject);
-                
-            }))
-            .catch(function(error){
-                console.log("error API random name o mail: "+error);
-            })
+                    // add to emailObject in the array
+                    this.arrayEmail.push(emailObject);
+                    
+                }))
 
-        console.log(this.arrayEmail)
+                .catch(function(error){
+                    console.log("error API random name o mail: "+error);
+                })
+        }
     },
 })
