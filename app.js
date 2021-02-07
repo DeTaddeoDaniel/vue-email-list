c730caff23c579d003fce53e5b17185503a01ff9
Daniel2711 <detaddeo.d@gmail.com>
2021-02-07 15:43:35 +0100

    Chiamate ajax per nome e mail tramite APi get

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 new Vue({
-    el: '#app',
+    el: "#app",
     data:{
-        test:'test'
-    }
+        test:'test',
+        arrayEmail: [],
+        NumMaxEmail: 10
+    },
+
+    beforeMount() {
+        
+        console.log('Crea 10 email');
+
+        let emailObject = {
+            name: '',
+            mail: ''
+        }
+
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/name')
+            .then(function (response) {
+                const result = response.data.response;
+                emailObject.name = result
+            })
+            .catch(function(error){
+                console.log("error API: "+error);
+            })
+        
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function (response) {
+                const result = response.data.response;
+                emailObject.mail = result
+            })
+            .catch(function(error){
+                console.log("error API: "+error);
+            })
+
+        console.log(emailObject)
+    },
 })
